0e8e49dfa8286ef04de655b2a35967782253d545
Robiul Islam <robi.cnits@gmail.com>
2023-05-07 20:12:00 +0600

    4th commit

---
+++ php-sum-of-two-variables/sum.php
@@ -0,0 +1,74 @@
+<?php
+
+//The Sum Practics
+$number = 20;
+$number = $number + 20;
+$number += 500;
+$number++;
+echo "<h1>The Sum Practics</h1>";
+echo $number;
+echo "<br>";
+
+//The Minus Practics
+
+$minus = 10;
+$minus2 = $minus - 40;
+echo "<h1>The minus Practics</h1>";
+echo $minus; 
+echo "<br>";
+$mins = 40;
+$mins -=10;
+echo $mins;
+echo "<br>";
+
+
+// The Product Practics
+$counts = 10;
+$count = 15;
+$count *= 40;
+echo "<h1>The count Practics</h1>";
+echo $count;
+echo "<br>";
+
+$counts *=10;
+echo $counts;
+echo "<br>";
+
+
+//The Product Shear
+
+$shear = 434;
+$shear2 = 10;
+echo "<h1>The shear Practics</h1>";
+echo $shear / $shear2; //forward slash backward slash
+echo "<br>";
+$shr = 420;
+$shr = 420 / 12; //forward slash backward slash
+echo $shr;
+echo "<br>";
+
+//Final exercise
+$shear3 = 5 % 42;
+echo "<h4>The shear Practics</h4>";
+echo $shear3; 
+echo "<br>";
+
+//First multiply 3 by 5 then subtract 12 from it.
+$shear4 = 12-3*5;
+echo $shear4;
+echo "<br>";
+
+//Brackets, Orders, Division, Multiplication, Addition and Subtraction
+
+$shar = (40-50) / 10;
+echo $shar;
+echo "<br>";
+
+$rora = (60-20) / (50-40);
+echo $rora;
+
+
+
+
+
+
