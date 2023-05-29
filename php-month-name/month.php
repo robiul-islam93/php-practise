93758fefecdfb2b3f2530ec9d3a1c73039049040
Robiul Islam <robi.cnits@gmail.com>
2023-05-29 17:46:55 +0600

    14th commit

---
+++ php-month-name/month.php
@@ -0,0 +1,61 @@
+<?php
+
+$Month_name= "march";
+switch($Month_name){
+    case "january":
+        $date = 20+11;
+        echo"It contains $date days.";
+    break;
+    case "february":
+        $date = 20+10;
+        echo"It contains $date days.";
+        break;
+        case "march":
+            $date = 20+11;
+        echo"It contains $date days.";
+            break;
+            case"April":
+                $date = 20+10;
+                echo"It contains $date days.";
+                break;
+            case"may":
+                $date = 20+11;
+                echo"It contains $date days.";
+                break;
+            case"june":
+                $date = 20+10;
+                echo"It contains $date days.";
+                break;
+            case"july":
+                $date = 20+11;
+                echo"It contains $date days.";
+                break;
+            case"august":
+                $date = 20+11;
+                echo"It contains $date days.";
+                break;
+            case"september":
+                $date = 20+10;
+                echo"It contains $date days.";
+                break;
+            case"october":
+                $date = 20+11;
+                echo"It contains $date days.";
+                break;
+            case"november":
+                $date = 20+10;
+                echo"It contains $date days.";
+                break;
+            case"december":
+                $date = 20+11;
+                echo"It contains $date days.";
+                break;
+                default:
+                echo "ERROR";
+}
+
+
+
+
+
+
